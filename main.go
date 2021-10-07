3980b2f5dbc5c58f2034dde8f36739a001bb46a3
irfanharfiansyah <irfanharfiansyah37@gmail.com>
2021-10-07 21:33:47 +0700

    Delete Todos

--- main.go
+++ main.go
@@ -30,6 +30,7 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/todos/:id", models.GetTodoById)
 	app.Post("/todos", models.CreateTodo)
 	app.Put("/todos/:id", models.UpdateTodo)
+	app.Delete("/todos/:id", models.DeleteTodo)
 }
 func main()  {
 	app := fiber.New()
